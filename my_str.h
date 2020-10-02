338195dc83b249b07dcfbad2d066498eb600afe4
Neyrad <70816457+Neyrad@users.noreply.github.com>
2020-10-02 19:59:03 +0300

    Add files via upload

---
+++ my_str.h
@@ -0,0 +1,91 @@
+int my_strlen (char* str);
+void my_strncopy (char* str2, char* str1, int size);
+char* my_strstr (char* str1, char* str2); 
+int my_strcmp (const void* stringA, const void* stringB); 
+char*  my_strtok (char* ptr, const char* token);
+char* my_strncat (char* str1, char* str2);
+
+//-----------------------------------------------------------------
+
+int my_strlen (char* str) {
+	int counter = 0;
+	for (counter = 0; *(str + counter) != '\0'; ++counter);
+	return counter + 1;
+}
+
+//-----------------------------------------------------------------
+
+void my_strncopy (char* str2, char* str1, int size) {
+	int counter = 0;
+	for (counter = 0; counter < size; ++counter)
+		*(str2 + counter) = *(str1 + counter);
+	*(str2 + counter - 1) = '\0';
+}
+
+//-----------------------------------------------------------------
+
+char* my_strstr (char* str1, char* str2) {
+	int i = 0, eqsymb = 0;
+	int len2 = my_strlen(str2);
+
+	for (i = 0; *(str1 + i) != '\0'; ++i) {
+		if (*(str1 + i) == *str2) {
+			for (int j = 0; *(str2 + j)     != '\0'
+						 && *(str1 + i + j) != '\0'; ++j) {
+				if (*(str1 + i + j) == *(str2 + j))
+					++eqsymb;
+				else
+					break;
+			}
+		}
+		if (eqsymb + 1 == len2)
+			return str1 + i;
+		else
+			eqsymb = 0;
+	}
+	return NULL;
+}
+
+//-----------------------------------------------------------------
+
+int my_strcmp (const void* stringA, const void* stringB) {
+	int i = 0;
+	for (i = 0; *((char*)stringA + i) != '\0' && *((char*)stringB + i) != '\0'; ++i) {
+		if      (*((char*)stringA + i) < *((char*)stringB + i))
+			return 2;
+		else if (*((char*)stringA + i) > *((char*)stringB + i))
+			return 1;
+	}
+	if      (*((char*)stringA + i) == '\0' && *((char*)stringB + i) == '\0')
+		return 0;
+	else if (*((char*)stringA + i) == '\0')
+		return -1;
+	else if (*((char*)stringB + i) == '\0')
+		return 1;
+	else
+		return ERROR;
+}
+
+//-----------------------------------------------------------------
+
+char*  my_strtok (char* ptr, const char* token) {
+	int counter = 0;
+	for (int j = 0; *(token + j) != '\0'; ++j) {
+		for (counter = 0; *(ptr + counter) != *(token + j)
+					   && *(ptr + counter) != '\0'; ++counter);
+		if (*(ptr + counter) == token) {
+			*(ptr + counter) = '\0';
+			return ptr;
+		}
+	}
+	return ptr;
+}
+
+//-----------------------------------------------------------------
+
+char* my_strncat (char* str1, char* str2) {
+	int counter = 0;
+	for (counter = 0; *(str1 + counter) != '\0'; ++counter);
+	for (int   i = 0; (*(str1 + counter + i) = *(str2 + i)) != '\0'; ++i);
+}
+
